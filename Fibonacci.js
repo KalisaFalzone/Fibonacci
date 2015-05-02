156236236464eed448ea423e29f20bfb4e45404b
KalisaFalzone <kalisa.falzone@gmail.com>
2015-05-02 15:58:22 -0700

    initializing repository

---
+++ Fibonacci.js
@@ -0,0 +1,26 @@
+
+var Fibonacci = function(numberofterms) {
+    a = 0
+    b = 1
+    i = 0
+    fibonacciarray = [1,1]
+	while (i < numberofterms-2) {
+  		c = a +b;
+		fibonacciarray.push(c);
+  		a = b;
+  		b = c;
+  		i += 1;
+	} 
+	if (numberofterms == 1) {
+		console.log([1]);
+	}
+	if (numberofterms == 2) {
+		console.log([1,1]);
+	}
+	else {
+	console.log(fibonacciarray);
+	}
+};
+
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# Fibonacci Numbers
+## Program by: Kalisa Falzone
+
+### Open up the html file in your browser (I used Google Chrome). From there the Javascript program can be run using the Javascript console in your web development tools. 
