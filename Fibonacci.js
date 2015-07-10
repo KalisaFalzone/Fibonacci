d9158120edf832e5a33ed8ff994257662612d82b
KalisaFalzone <kalisa.falzone@gmail.com>
2015-07-10 09:35:01 -0700

    added recursive version

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,24 +1,13 @@
+var Fibonacci = function(n) {
 
-var Fibonacci = function(numberofterms) {
-    a = 0
-    b = 1
-    i = 0
-    fibonacciarray = [1,1]
-	while (i < numberofterms-2) {
-  		c = a +b;
-		fibonacciarray.push(c);
-  		a = b;
-  		b = c;
-  		i += 1;
-	} 
-	if (numberofterms == 1) {
-		console.log([1]);
-	} else if (numberofterms == 2) {
-		console.log([1,1]);
-	} else {
-	console.log(fibonacciarray);
-	}
+  if (n === 0) {
+    accumulator = 0;
+    return 0;}
+  if (n === 1) {return 1;}
+  if (n === 2) {return 1;}
+
+  accumulator = nthFibonacci(n-1) + nthFibonacci(n-2);
+
+  return accumulator;
+
 };
-
-
-
